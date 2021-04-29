7a959236e1c63173fb26b9bfceea30c0b3df2990
Victor Rosas <victor.rosas.96@hotmail.com>
2021-04-29 15:58:48 -0500

    build

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { menuData } from '../../data/MenuData';
-import logoimg from '../../assets/logo_light.png';
 
-import { Nav, Logo, MenuBars, NavMenu, NavMenuLinks, NavBtn } from './styles';
-import { Button } from '../Button/Button';
+import { Nav, Logo, MenuBars, NavMenu, NavMenuLinks } from './styles';
 
 const Navbar = () => {
   return (
